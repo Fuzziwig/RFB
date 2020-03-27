31cb29a8467af79bb33817310ffd7e27ca668ba4
FrederikKea <fred1952@stud.kea.dk>
2020-03-27 20:26:05 +0100

    Employee CRUD virker

---
+++ src/Controllers/EmployeeController.java
@@ -0,0 +1,103 @@
+package Controllers;
+
+import Models.Employee;
+import Service.EmployeeService;
+
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class EmployeeController {
+
+    EmployeeService es = new EmployeeService();
+
+    public void createEmployee() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Medarbejdernummer: ");
+        int employee_id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Fornavn på medarbejder: ");
+        String employee_first_name = sc.nextLine();
+
+        /*
+        Skal måske bruge dette også - Son
+        //Har sat en ekstra sc.nextLine() her da der en bug i java som gør den ellers
+        //ville have spruget "Fornavn på barn" over.
+        sc.nextLine();
+        */
+
+        System.out.println("Efternavn på medabejder: ");
+        String employee_last_name = sc.nextLine();
+        //child.setChildFirstName(firstName);
+
+        System.out.println("Jobstilling: ");
+        String employee_title = sc.nextLine();
+        //child.setChildFirstName(lastName);
+
+        System.out.println("Telefonnummer: ");
+        String employee_phone_number = sc.nextLine();
+        //Stue s;
+
+        System.out.println("Email: ");
+        String employee_email = sc.nextLine();
+
+        System.out.println("Vagtplannummer: ");
+        int workplan_id = sc.nextInt();
+
+        System.out.println("Adresse: ");
+        int address_id = sc.nextInt();
+
+        Employee employee = new Employee(employee_id, employee_first_name, employee_last_name,employee_title, employee_phone_number, employee_email,workplan_id, address_id);
+        es.createEmployee(employee);
+    }
+
+    public void readEmployee() throws SQLException {
+        es.readEmployee();
+    }
+
+    public void updateEmployee() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nyt nummer på medarbejder: ");
+        int employee_id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Fornavn på medarbejder: ");
+        String employee_first_name = sc.nextLine();
+
+        System.out.println("Efternavn på medabejder: ");
+        String employee_last_name = sc.nextLine();
+        //child.setChildFirstName(firstName);
+
+        System.out.println("Jobstilling: ");
+        String employee_title = sc.nextLine();
+        //child.setChildFirstName(lastName);
+
+        System.out.println("Telefonnummer: ");
+        String employee_phone_number = sc.nextLine();
+        //Stue s;
+
+        System.out.println("Email: ");
+        String employee_email = sc.nextLine();
+
+        System.out.println("Vagtplannummer: ");
+        int workplan_id = sc.nextInt();
+
+        System.out.println("Adresse: ");
+        int address_id = sc.nextInt();
+        Employee employee = new Employee(employee_id, employee_first_name, employee_last_name,employee_title, employee_phone_number, employee_email,workplan_id, address_id);
+        es.updateEmployee(employee);
+    }
+    public void deleteEmployee() throws SQLException {
+        System.out.println("Hvilket medarbejder vil du gerne slette fra databasen? ");
+        System.out.println("indtast medarbejdernummer på ansatte for at slette medarbejder");
+        Scanner sc = new Scanner(System.in);
+        int employee_id = sc.nextInt();
+        System.out.println("Du har nu slettet medarbejder fra databasen");
+        es.deleteEmployee(employee_id);
+    }
+}
+
+//resultsets
+//preparedstatement
---
+++ src/Repositories/EmployeeRepository.java
@@ -0,0 +1,64 @@
+package Repositories;
+
+import Config.MySQLConnection;
+
+import java.sql.*;
+
+public class EmployeeRepository {
+
+    private Connection con;
+
+    public EmployeeRepository() {
+        MySQLConnection msc = new MySQLConnection();
+        this.con = msc.create();
+    }
+
+    public void createEmployee(int employee_id, String employee_first_name, String employee_last_name, String employee_title, String employee_phone_number, String employee_email, int workplan_id, int address_id) throws SQLException {
+        String sql ="INSERT INTO employee_table(employee_id, employee_first_name, employee_last_name, employee_title, employee_phone_number, employee_email, workplan_id, address_id)"+"VALUES (?,?,?,?,?,?,?,?)";
+        //A SQL statement is precompiled and stored in a PreparedStatement object. This object can then be used to efficiently execute this statement multiple times.
+        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        preparedStatement.setInt(1,employee_id);
+        preparedStatement.setString(2,employee_first_name);
+        preparedStatement.setString(3,employee_last_name);
+        preparedStatement.setString(4,employee_title);
+        preparedStatement.setString(5,employee_phone_number);
+        preparedStatement.setString(6,employee_email);
+        preparedStatement.setInt(7,workplan_id);
+        preparedStatement.setInt(8,address_id);
+        preparedStatement.execute();
+        preparedStatement.close();
+    }
+    //Execute a query
+    public ResultSet readEmployee() throws SQLException {
+        String sql = "SELECT * FROM rfb.employee_table";
+        Statement stmt = con.createStatement();
+        try {
+            ResultSet rs = stmt.executeQuery(sql);
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public void updateEmployee(int employee_id, String employee_first_name, String employee_last_name, String employee_title, String employee_phone_number, String employee_email, int workplan_id, int address_id) throws SQLException {
+        String sql = "UPDATE rfb.employee_table SET employee_first_name=?, employee_last_name=?, employee_title=?, employee_phone_number=?, employee_email=?, workplan_id=?, address_id=? WHERE employee_id=?";
+        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        preparedStatement.setString(1,employee_first_name);
+        preparedStatement.setString(2,employee_last_name);
+        preparedStatement.setString(3,employee_title);
+        preparedStatement.setString(4,employee_phone_number);
+        preparedStatement.setString(5,employee_email);
+        preparedStatement.setInt(6,workplan_id);
+        preparedStatement.setInt(7, address_id);
+        preparedStatement.setInt(8, employee_id);
+        preparedStatement.execute();
+        preparedStatement.close();
+    }
+    public void deleteEmployee(int employee_id) throws SQLException {
+        String sql = "DELETE FROM rfb.employee_table WHERE employee_id=?";
+        PreparedStatement preparedStatement = con.prepareStatement(sql);
+        preparedStatement.setInt(1,employee_id);
+        preparedStatement.execute();
+        preparedStatement.close();
+    }
+}
